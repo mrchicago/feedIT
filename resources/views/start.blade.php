fd98ee9807a6c7449d3260251cab474896102f9d
Nils Fischer <bluecraank@outlook.de>
2024-08-27 07:31:23 +0000

    Update

---
+++ resources/views/start.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="card mt-5">
+        <h5 class="card-header">Mitbestellen</h5>
+        <div class="card-body">
+            <h5 class="card-title">Bestelltag {{ date('d.m.Y') }}</h5>
+            <p class="card-text">Fein lecker Pizza oder so</p>
+            <a href="/mitbestellen" class="btn btn-primary">Jetzt mitbestellen</a>
+        </div>
+    </div>
+
+    <div class="text-center mt-5">
+        <div class="row">
+            <div class="col">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">⌀ Bestellungen pro Woche</h5>
+                        <h1>3</h1>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">⌀ Besteller pro Bestellung</h5>
+                        <h1>6</h1>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">⌀ Preis pro Bestellung</h5>
+                        <h1>6,50€</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,30 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LieferantenController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('start  ');
+})->name('start');
+
+Route::prefix('lieferanten')->group(function () {
+    Route::get('/', [LieferantenController::class, 'index'])->name('lieferanten');
+    Route::get('/create', [LieferantenController::class, 'create'])->name('lieferanten.create');
+    Route::post('/store', [LieferantenController::class, 'store'])->name('lieferanten.store');
+    Route::get('/edit/{id}', [LieferantenController::class, 'edit'])->name('lieferanten.edit');
+    Route::post('/update/{id}', [LieferantenController::class, 'update'])->name('lieferanten.update');
+    Route::get('/delete/{id}', [LieferantenController::class, 'delete'])->name('lieferanten.delete');
 });
-Route::get('/test', function () {
-    return view('welcome');
+
+Route::prefix('gerichte')->group(function () {
+    Route::get('/', [GerichteController::class, 'index'])->name('gerichte');
+    Route::get('/create', [GerichteController::class, 'create'])->name('gerichte.create');
+    Route::post('/store', [GerichteController::class, 'store'])->name('gerichte.store');
+    Route::get('/edit/{id}', [GerichteController::class, 'edit'])->name('gerichte.edit');
+    Route::post('/update/{id}', [GerichteController::class, 'update'])->name('gerichte.update');
+    Route::get('/delete/{id}', [GerichteController::class, 'delete'])->name('gerichte.delete');
 });
-Route::get('/order', [LieferantenController::class, 'show']);
+
+Route::prefix('bestellungen')->group(function () {
+    Route::get('/mitbestellen', [BestellungenController::class, 'index'])->name('mitbestellen');
+    Route::post('/mitbestellen', [BestellungenController::class, 'store'])->name('mitbestellen.store');
+
+    Route::get('/', [BestellungenController::class, 'index'])->name('bestellungen');
+});
